0bd4fcfaf600c8bb3ec53e65fd7bf30b7f4306f0
asadraiyan <asadraiyan001@gmail.com>
2023-06-09 20:42:03 +0530

    removed extra spaces and comment off consoles

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ let reset = document.getElementById("reset")
 
 for (listItems of lists) {
     console.log("listItems =", listItems)
-    listItems.addEventListener("dragstart", function (e) {
+    listItems.addEventListener("dragstart", function (e) { // function to start the drag
         let selected = e.target;
-        console.log("selected =", selected)
-        console.log("Drag start")
 
-        console.log("rightBox =", rightBox)
+        // console.log("Drag start")
 
-        rightBox[0].addEventListener("dragover", function (e) {
+        rightBox[0].addEventListener("dragover", function (e) { // function to drag the item
             e.preventDefault();
-            console.log("Drag over")
+
+            // console.log("Drag over")
         })
 
-        rightBox[0].addEventListener("drop", function (e) {
+        rightBox[0].addEventListener("drop", function (e) { // function to drop the items
             rightBox[0].appendChild(selected);
             selected = null;
-            console.log("Drag drop")
+
+            // console.log("Drag drop")
+
             setTimeout(() => {
                 window.alert("Item moved successfully")
             }, 150);
@@ -30,6 +31,6 @@ for (listItems of lists) {
 
 }
 
-reset.onclick = () => {
+reset.onclick = () => {  // function  to clear the second container and reset the first container to its original state.
     window.location.reload();
 }
